dc09fc1259f52b1ea0493f8a1bdd32929898e0c4
Mister Sandman <onoshkosasha99@gmail.com>
2019-02-26 13:27:15 +0200

    Created "checkStudent" method.

--- app.js
+++ app.js
@@ -18,28 +18,47 @@ class University extends Student {
     }
     addStudent(name){
         this.studentsList.push(name);
+        console.log(this.studentsList);
     }
 }
 class Internship extends University {
     constructor(name){
         super();
         this.name=name;
+        this.suitableStudents=[];
     }
 
     getStudent(){
-
+        return this.suitableStudents;
     }
-    checkStudent(){
+    checkStudent(university){
+        university.studentsList.forEach((university)=>{
+            if(university.level.level!==null && university.level.level>=4) {
+                this.suitableStudents.push(university.name);
+            }
 
+        });
     }
 }
+
+
+let high_lvl_knowledge = new Knowledge("5");
+let mid_lvl_knowledge = new Knowledge("4");
+let low_lvl_knowledge = new Knowledge("3");
+
+let some_student = new Student("Sasha Onoshko", high_lvl_knowledge);
 
 let university = new University("CH.U.I.");
 university.addStudent(new Student("Andrew Kostenko", low_lvl_knowledge));
 university.addStudent(new Student("Julia Veselkina", high_lvl_knowledge));
 university.addStudent(new Student("Maria Perechrest", mid_lvl_knowledge));
 
+university.addStudent(some_student);
+console.log(university.studentsList);
+
 let internship = new Internship("Interlink");
+
+internship.checkStudent(university);
 
 console.log("List of internship's students:");
 console.log(internship.getStudent());
