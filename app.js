605690987a28ba7ad8eb5e29b2be3069ebfb1e19
Mister Sandman <onoshkosasha99@gmail.com>
2019-02-26 13:24:21 +0200

    Filled in basic constructors.

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
 class Knowledge {
     constructor(level='') {
-
+        this.level=level;
     }
 }
 class Student extends Knowledge {
     constructor(name, level){
         super(level);
-
+        this.name=name;
     }
 
 }
 class University extends Student {
     constructor(name) {
         super();
-
+        this.name=name;
+        this.studentsList=[];
     }
     addStudent(name){
-
+        this.studentsList.push(name);
     }
 }
 class Internship extends University {
     constructor(name){
         super();
+        this.name=name;
+    }
+
+    getStudent(){
 
     }
-    getStudent(){
+    checkStudent(){
 
     }
 }
@@ -34,8 +39,8 @@ university.addStudent(new Student("Andrew Kostenko", low_lvl_knowledge));
 university.addStudent(new Student("Julia Veselkina", high_lvl_knowledge));
 university.addStudent(new Student("Maria Perechrest", mid_lvl_knowledge));
 
-
 let internship = new Internship("Interlink");
+
 console.log("List of internship's students:");
 console.log(internship.getStudent());
 
